1eac1e1c536f29985cf4a80269d7227cc5c3f1f6
Daniel Kang <danieldonghoonkang@gmail.com>
2019-10-04 19:11:55 -0700

    created request handler that uses axios to fetch data from coindesk

--- challenge_2/server/index.js
+++ challenge_2/server/index.js
@@ -1,2 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/bitcoinPrice', (req, res) => {
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+  .then(data => res.status(200).send(data.data.bpi))
+  .catch(err => res.status(500).send(err.stack));
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
